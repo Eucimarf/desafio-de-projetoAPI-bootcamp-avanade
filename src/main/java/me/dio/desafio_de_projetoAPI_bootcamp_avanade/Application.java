8b2fa2e09ec157895e68e7ac4b389f76e1481c64
Eucimarf <eucimarizabel@gmail.com>
2025-03-30 11:50:59 -0300

    Implementação das classes no domain e conexão com o banco de dados

--- src/main/java/me/dio/desafio_de_projetoAPI_bootcamp_avanade/Application.java
+++ src/main/java/me/dio/desafio_de_projetoAPI_bootcamp_avanade/Application.java
@@ -2,8 +2,10 @@ package me.dio.desafio_de_projetoAPI_bootcamp_avanade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "me.dio.domain.model")
 public class Application {
 
 	public static void main(String[] args) {
